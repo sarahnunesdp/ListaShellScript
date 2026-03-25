c845df262660d01176e203543097570919cd266f
Sarah <nunesdopradosarah@gmail.com>
2026-03-25 20:16:19 -0300

    Update EX_04.sh

--- LISTA01/EX_04.sh
+++ LISTA01/EX_04.sh
@@ -3,7 +3,7 @@
 read -p "Informe o caminho do arquivo" caminho
 read -p "Informe o diretório de destino" destino
 
-mv $caminho $destino
+mv "$caminho" "$destino"
 
 echo "Arquivo movido com sucesso"
 
